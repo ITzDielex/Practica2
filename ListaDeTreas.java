cad90ef3e35895265484766086eb6a9de503c9b2
ITzDielex <143724094+ITzDielex@users.noreply.github.com>
2023-09-05 09:48:47 -0500

    Add files via upload

---
+++ ListaDeTreas.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author labcca
+ */
+public class ListaDeTreas {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[]args) {
+        // TODO code application logic here
+        int tarea; 
+        String tarea1 = "Pasear a el perro";
+        String tarea2 = "hacer la tarea";
+        String tarea3 = "lavar el carro";
+        String tarea4 = "lavar los trastes";
+        String tarea5 = "lavar la ropa";
+        
+        System.out.println("Mi mama me pido " + tarea1);
+        System.out.println("Mi mama me pido " + tarea2);
+        System.out.println("Mi mama me pido " + tarea3);
+        System.out.println("Mi mama me pido " + tarea4);
+        System.out.println("Mi mama me pido " + tarea5);
+    }
+}
