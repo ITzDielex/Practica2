0af92a79d44b40ae653d86826e378782b21fe4b3
ITzDielex <143724094+ITzDielex@users.noreply.github.com>
2023-09-07 08:46:45 -0600

    Add files via upload

---
+++ Practica2.java
@@ -0,0 +1,18 @@
+
+package operaciones;
+public class Practica2 {
+    public static void main (String[]args) {
+        System.out.println("Diego Alejandro Reyes Medina");
+        int base = 10;
+        int altura = 5;
+        int areaRectanguloEntero = base * altura;
+        
+        System.out.println("El area del rectangulo es: " + areaRectanguloEntero);
+        
+        double baseA = 10.5;
+        double alturaB = 15.5;
+        double areaRectanguloDecimal = baseA * alturaB;
+        
+        System.out.println("El area del rectangulo en decimal es: " + areaRectanguloDecimal);
+    }
+}
